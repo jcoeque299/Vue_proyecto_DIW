2de0d58355f7c1f486a81f25eb841913ca349572
Judith <jcoeque299@g.educaand.es>
2024-02-23 13:38:58 +0100

    Implementado formulario de busqueda

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,10 +1,48 @@
-<script setup>
-    import SearchForm from "../components/SearchForm.vue"
-    import SearchResults from "../components/SearchResults.vue"
-</script>
-
 <template>
     <p>Search</p>
-    <SearchForm/>
-    <SearchResults/>
+    <form @submit.prevent="searchEvents">
+        <label for="eventQuery">Evento</label>
+        <input type="text" id="eventQuery" name="eventQuery" v-model="eventQuery">
+        <label for="eventCountry">Codigo de pais</label>
+        <select id="eventCountry" name="eventCountry" v-model="eventCountry">
+            <option v-for="country in countryData" v-bind:value="country.code">{{ country.name }}</option>
+        </select>
+        <button type="button" v-on:click="searchEvents">Buscar</button>
+    </form>
+    <ul v-if="events">
+        <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+        </li>
+    </ul>
+    <p v-else>No hay resultados</p>
 </template>
+
+<script>
+    import countryData from "/src/data.json"
+    export default {
+        data() {
+            return {
+                events: null,
+                eventQuery: "",
+                eventCountry: "",
+                countryData: countryData
+            }
+        },
+        methods: {
+            async searchEvents() {
+                const data = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${eventQuery.value}&countryCode=${eventCountry.value}&apikey=S1sDAS05dZI5JmtvdarQaZN5tFxkOUpr`)
+                const results = await data.json()
+                if (results._embedded) {
+                    this.events = results._embedded.events
+                }
+            },
+        }
+    }
+    // const searchEvents = async(searchData) => {
+    //     const data = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${searchData}&apikey=S1sDAS05dZI5JmtvdarQaZN5tFxkOUpr`)
+    //     let events = await data.json()
+    //     results = events
+    //     console.log(results)
+    // }
+
+</script>

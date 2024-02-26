08173b5bcf1f6104a854bbc6bc04ec0e1162c36e
Judith <jcoeque299@g.educaand.es>
2024-02-26 13:29:19 +0100

    Ampliados detalles de eventos y formulario de busqueda

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,5 +1,16 @@
 <template>
-    <p v-if="event">{{ event.name }}</p>
+    <section v-if="event">
+        <h1>
+        {{ event.name }}
+        </h1>
+        <img v-if="event.images" :src="event.images[0].url">
+        <img v-if="event.seatmap" :src="event.seatmap.staticUrl">
+        <p v-if="!event.dates.start.dateTBD && !event.dates.start.dateTBA">Fecha del evento: {{ event.dates.start.localDate }}</p>
+        <p v-if="event._embedded.venues">En: {{ event._embedded.venues[0].country.name }}, {{ event._embedded.venues[0].city.name }}</p>
+        <p v-if="event.info">{{ event.info }}</p>
+        <button v-if="event.seatmap">Ver mapa del recinto</button>
+        <a v-if="event.url" :href="event.url" target="_blank">Comprar entradas</a>
+    </section>  
 </template>
 
 <script>
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -2,11 +2,24 @@
     <p>Search</p>
     <form @submit.prevent="searchEvents">
         <label for="eventQuery">Evento</label>
-        <input type="text" id="eventQuery" name="eventQuery" v-model="eventQuery">
-        <label for="eventCountry">Codigo de pais</label>
+        <input type="text" id="eventQuery" name="eventQuery" v-model="eventQuery" placeholder="Cualquiera">
+        <label for="eventCountry">Pais</label>
         <select id="eventCountry" name="eventCountry" v-model="eventCountry">
+            <option value="" selected>-Cualquiera-</option>
             <option v-for="country in countryData" v-bind:value="country.code">{{ country.name }}</option>
         </select>
+        <label for="eventType">Tipo de evento</label>
+        <select id="eventType" name="eventType" v-model="eventType">
+            <option value="" selected>-Cualquiera-</option>
+            <option value="Sports">Deportes</option>
+            <option value="Music">Música</option>
+            <option value="Arts & Theatre">Artes y teatro</option>
+            <option value="Film">Cine</option>
+            <option value="Merchandise">Merchandising</option>
+            <option value="Donation">Donaciones</option>
+        </select>
+        <label for="eventDate">Fecha</label>
+        <input type="date" id="eventDate" name="eventDate" v-model="eventDate">
         <button type="button" v-on:click="searchEvents">Buscar</button>
     </form>
     <ul v-if="events">
@@ -25,26 +38,23 @@
         data() {
             return {
                 events: null,
+                eventType: "",
                 eventQuery: "",
                 eventCountry: "",
+                eventDate: `${new Date().getFullYear()}-01-01`,
                 countryData: countryData
             }
         },
         methods: {
             async searchEvents() {
-                const data = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${eventQuery.value}&countryCode=${eventCountry.value}&apikey=S1sDAS05dZI5JmtvdarQaZN5tFxkOUpr`)
+                const data = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${this.eventQuery}&countryCode=${this.eventCountry}&classificationName=${this.eventType}&startDateTime=${this.eventDate}T00:00:00Z&apikey=S1sDAS05dZI5JmtvdarQaZN5tFxkOUpr`)
                 const results = await data.json()
                 if (results._embedded) {
                     this.events = results._embedded.events
+                    return
                 }
+                this.events = null
             },
         }
     }
-    // const searchEvents = async(searchData) => {
-    //     const data = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?keyword=${searchData}&apikey=S1sDAS05dZI5JmtvdarQaZN5tFxkOUpr`)
-    //     let events = await data.json()
-    //     results = events
-    //     console.log(results)
-    // }
-
 </script>
